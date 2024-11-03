91008658df29c1443d552d4443d04545bfe6282b
mikl10 <46415270+mikl10@users.noreply.github.com>
2024-11-03 13:16:23 +0100

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'http://mickael.base.eth';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xbfdf450C167847aFDdA5b099d279dBEF133D4A05';
